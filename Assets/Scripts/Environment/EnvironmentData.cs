a831d8fe3e28fe185866290fb36c8f9dddcf14a3
Ali <ali.valizadeh.h@gmail.com>
2020-08-02 20:07:34 +0200

    Everything of the main challenge except expert bonus feature

---
+++ Assets/Scripts/Environment/EnvironmentData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public static class EnvironmentData
+    {
+        public const float GravityForce = 9.8f;
+        public static readonly Vector3 InitialGravity = Physics.gravity;
+    }
+}
